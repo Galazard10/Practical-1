e4c82cb5e73388ec9496dd9d2007a7ce7c70d48e
Galazard10 <galym.ilyas.2002@gmail.com>
2022-02-01 14:11:54 +0600

    Practical-1 Spring Boot Setup

---
+++ src/main/java/com/example/practical1/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.practical1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("myvar", "Says hello to the people");
+        return "index";
+    }
+}
